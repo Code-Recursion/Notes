69d8edc2e1669a2ac631a89909183541ac4fcf7d
Ajay Singh <helloajaysingh1@gmail.com>
2021-01-25 17:31:17 +0530

    mongodb set up

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const app = express();
 
-// show static content using express's built-in middleware
-app.use(express.static('build'))
+// show static content React) using express's built-in middleware
+app.use(express.static("build"));
 
-//cors
+//cors cross origin resource sharing
 const cors = require("cors");
 app.use(cors());
 
 // json-parser
 app.use(express.json());
 
-// middleware
+// dot env library
+require("dotenv").config();
+
+// note model
+const Note = require("./models/note");
+
+// middleware for logging between req and res
 const requestLogger = (request, response, next) => {
   console.log("Method", request.method);
   console.log("Path", request.path);
@@ -20,93 +26,102 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unkown endpoint" });
-};
-
-// middleware
+// calling request logger middleware
 app.use(requestLogger);
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is not a programming language",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true,
-  },
-  {
-    id: 4,
-    content: "useless note",
-    date: "2019-05-30T19:20:14.298Z",
-    important: false,
-  },
-];
-
-app.get("/", (request, response) => {
-  response.send("<h1>hello world</h1>");
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes.map((n) => n.toJSON()));
+  });
 });
 
-app.get("/api/notes/", (request, response) => {
-  response.json(notes);
-});
+app.get("/api/notes/:id", (request, response, next) => {
+  //using mongoose's findById method for fetching individual notes
 
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note).end();
+      } else {
+        response.status(404).end();
+      }
+    })
+    // he error that is passed forwards is given to the next function as a parameter. If next was called without a parameter, then the execution would simply move onto the next route or middleware. If the next function is called with a parameter, then the execution will continue to the error handler middleware.
+    .catch((error) => next(error));
+  // errorHandler defined at the bottom
 });
 
 app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id != id);
-  response.status(204).end();
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
-  return maxId + 1;
-};
-
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: "content is missing",
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
+  });
+
+  note
+    .save()
+    .then((savedNote) => savedNote.json())
+    .then((savedAndJsonForamttedNote) => {
+      response.json(savedAndJsonForamttedNote);
+    })
+    .catch((error) => next(error));
+});
+
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
+  const note = {
+    content: body.content,
+    important: body.important,
   };
 
-  notes = notes.concat(note);
-  console.log(note);
-  response.json(note);
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
+
+// if unknown end point is hit other than the ones defined above end points, hence calling this at bottom level
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unkown endpoint" });
+};
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+// express error handler
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  // if the request is malformatted the findById will throw an error causing promise to be rejected
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformattted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`http://localhost:${PORT}`);
